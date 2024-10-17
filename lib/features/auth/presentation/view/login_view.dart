0ccd35c9a9e376822cadb38320d0a2835db17d42
Abdelrahman Ahmed <101799276+Abdelrahman-Ahmed-2003@users.noreply.github.com>
2024-10-17 18:40:36 +0300

    Update login_view.dart

--- lib/features/auth/presentation/view/login_view.dart
+++ lib/features/auth/presentation/view/login_view.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/features/auth/presentation/view/widgets/login_body.dart';
 import 'package:flutter/material.dart';
 
 class LoginView extends StatelessWidget {
@@ -5,8 +6,11 @@ class LoginView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child:Text('Login')),
+    return SafeArea(
+      child: Scaffold(
+          body: 
+          LoginBody(),
+        ),
     );
   }
 }
