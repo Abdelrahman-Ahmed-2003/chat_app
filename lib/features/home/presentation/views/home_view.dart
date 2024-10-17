c9234b9e3f46e55ab606a05ac0ebf6c754595cff
abdelrahman <abdo12345678mo@gmail.com>
2024-10-17 18:41:54 +0300

    delete repeated files

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -5,6 +5,9 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+        child: Scaffold(
+      body: HomeBody(),
+    ));
   }
 }
